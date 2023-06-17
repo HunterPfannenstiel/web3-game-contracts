eb6af5d3325d6a225b651f0869f8bab84a296bac
ohShews <pfannenstielhunter@gmail.com>
2023-06-17 14:11:05 -0500

    Faucet contracts are now deployed and verified on goerliscan

--- Faucet/migrations/1_Faucet.js
+++ Faucet/migrations/1_Faucet.js
@@ -1,7 +1,7 @@
-const Faucet = artifacts.require("MachoFaucet");
-const MachoUSD = artifacts.require("MachoUSD");
-const MachoMagic = artifacts.require("MachoMagic");
-const MachoCoin = artifacts.require("MachoCoin");
+const Faucet = artifacts.require("MachoFaucet"); //0x76dE2003a8Ed56fcd27Cb469Afda083E8817f609
+const MachoUSD = artifacts.require("MachoUSD"); //0x905DadcAC06C5fBe50D176B54ED062f804dfBE1C
+const MachoMagic = artifacts.require("MachoMagic"); //0xC11F0F3E7F18747f7A8cf9fDB371B40d2962083f
+const MachoCoin = artifacts.require("MachoCoin"); //0x9D85F23FA51c6690a019ad79F042B313EA618697
 
 module.exports = async (deployer) => {
   await deployer.deploy(MachoUSD);
--- Faucet/truffle-config.js
+++ Faucet/truffle-config.js
@@ -1,10 +1,20 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const keys = require("./keys.json");
+
 module.exports = {
-  contracts_build_factory: "./contracts",
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "5777",
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(keys.KEY, keys.INFURA_URL),
+      network_id: "5",
+      gas: 5500000,
+      gasPrice: 20000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
     },
   },
 
@@ -13,4 +23,8 @@ module.exports = {
       version: "0.8.17",
     },
   },
+  api_keys: {
+    etherscan: keys.ETHERSCAN,
+  },
+  plugins: ["truffle-plugin-verify"],
 };
